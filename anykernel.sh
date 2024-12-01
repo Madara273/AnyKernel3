2347b391fe6c01554cb1340b4d48026765a62a9a
Madara273 <ravenhoxs@gmail.com>
2024-12-01 09:31:30 +0000

    add anykernel for sakura core
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>

---
+++ anykernel.sh
@@ -0,0 +1,72 @@
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
+# Sakura changes by Madara273
+
+### AnyKernel setup
+# global properties
+properties() { '
+kernel.string=SAKURA_NH
+kernel.compiler=GCC EVA/Clang Random
+kernel.made=Madara273
+message.word=Message me at @Madara_273c --- t.me/Madara_273c
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=OnePlus9Pro
+device.name2=lemonadep
+device.name3=OnePlus9ProTMO
+device.name4=lemonadept
+device.name5=OnePlus9
+device.name6=lemonade
+device.name7=OnePlus9TMO
+device.name8=lemonadet
+device.name9=OnePlus9VZW
+device.name10=lemonadev
+supported.versions=15 - 16
+'; } # end properties
+
+### AnyKernel install
+## trim partitions
+fstrim -v /data;
+
+### AnyKernel install
+## boot files attributes
+boot_attributes() {
+set_perm_recursive 0 0 755 644 $RAMDISK/*;
+set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
+} # end attributes
+
+# boot shell variables
+BLOCK=auto;
+IS_SLOT_DEVICE=auto;
+RAMDISK_COMPRESSION=auto;
+PATCH_VBMETA_FLAG=auto;
+
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh;
+
+# boot install
+dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+
+# remove old root patch avoidance hack
+patch_cmdline "skip_override" "";
+
+write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+## end boot install
+
+# vendor_boot shell variables
+BLOCK=vendor_boot;
+IS_SLOT_DEVICE=auto;
+RAMDISK_COMPRESSION=auto;
+PATCH_VBMETA_FLAG=auto;
+
+# reset for vendor_boot patching
+reset_ak;
+
+# vendor_boot install
+dump_boot; # use split_boot to skip ramdisk unpack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
+
+write_boot; # use flash_boot to skip ramdisk repack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
+## end vendor_boot install
