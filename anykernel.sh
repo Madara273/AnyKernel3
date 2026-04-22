a10f220a6acc3e7313fd64d1d430b7649aca5938
Madara273 <ravenhoxs@gmail.com>
2026-04-22 05:34:43 +0300

    anykernel.sh: no android version
    Signed-off-by: Madara273 <ravenhoxs@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -15,7 +15,7 @@ do.cleanup=1
 do.cleanuponabort=0
 device.name1=spes
 device.name2=spesn
-supported.versions=15 - 16
+supported.versions=
 supported.patchlevels=
 supported.vendorpatchlevels=
 '; } # end properties
